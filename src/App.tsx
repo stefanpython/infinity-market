d2a4f8af678dd5b7109bb155bd5cefa17efbfdbd
stefanpython <stefanhtml@yahoo.com>
2024-06-24 16:28:21 +0300

    Link productDetails component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 import { CarouselDefault } from "./components/Carousel";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Shop from "./components/Shop";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<CarouselDefault />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
         </Routes>
 
         <Footer />
